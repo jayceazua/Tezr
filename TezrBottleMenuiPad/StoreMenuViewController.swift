fa58b06e690a60802464d0cca5acdaafccf92093
LinnierGames <esericksanc@gmail.com>
2018-06-18 09:07:09 -0700

    Layout Colors in MenuVc

--- TezrBottleMenuiPad/StoreMenuViewController.swift
+++ TezrBottleMenuiPad/StoreMenuViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class StoreMenuViewController: UIViewController {
     
+    private var menu = StoreMenu.localStore
+    
+//    private var horz
+    
     // MARK: - RETURN VALUES
     
     // MARK: - VOID METHODS
+    
+    private func initLayout() {
+        let horzStackView = UIStackView(
+            axis: .horizontal,
+            distribution: .fillEqually,
+            alignment: .fill
+        )
+        
+        // create collection views for each category
+        let colors: [UIColor] = [.red, .blue, .green, .yellow]
+        for aColor in colors {
+            let colorView = UIView()
+            colorView.translatesAutoresizingMaskIntoConstraints = false
+            colorView.backgroundColor = aColor
+            
+            //add to a stack view
+            horzStackView.addArrangedSubview(colorView)
+        }
+        
+        // layout stack view in scrollview to allow paging through the collection views
+        self.scrollViewMenu.addSubview(horzStackView)
+        horzStackView.topAnchor.constraint(equalTo: scrollViewMenu.topAnchor)
+        horzStackView.leadingAnchor.constraint(equalTo: scrollViewMenu.leadingAnchor)
+        horzStackView.trailingAnchor.constraint(equalTo: scrollViewMenu.trailingAnchor)
+        horzStackView.bottomAnchor.constraint(equalTo: scrollViewMenu.bottomAnchor)
+        horzStackView.heightAnchor.constraint(equalTo: scrollViewMenu.heightAnchor)
+        horzStackView.widthAnchor.constraint(equalTo: scrollViewMenu.widthAnchor, multiplier: CGFloat(colors.count))
+        NSLayoutConstraint.activate(horzStackView.constraints)
+    }
     
     /*
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -40,6 +73,7 @@ class StoreMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initLayout()
     }
 
 }
